89b347812e3c3618c29a0f009453d32c53bd8faa
Tobias Cadee <tobias.cadee@ticketswap.com>
2023-12-04 14:39:10 +0100

    feat: :sparkles: move from stripe python sdk to rest implementation
    stripe api is very easy to use so I moved from the stripe python library to the rest implementation.

--- tap_stripe/client.py
+++ tap_stripe/client.py
@@ -10,11 +10,20 @@ from urllib.parse import urlencode
 from datetime import datetime
 from urllib.parse import parse_qsl
 import requests
-import stripe
+import logging
+import json
+import typing
 
 from singer_sdk.tap_base import Tap
-from singer_sdk.streams import Stream
+from singer_sdk.streams import RESTStream
+from singer_sdk.authenticators import BasicAuthenticator
+from singer_sdk.pagination import BaseOffsetPaginator
 
+if typing.TYPE_CHECKING:
+    from requests import Response
+
+T = typing.TypeVar("T")
+TPageToken = typing.TypeVar("TPageToken")
 
 if sys.version_info >= (3, 8):
     from functools import cached_property
@@ -24,18 +33,73 @@ else:
 _Auth = Callable[[requests.PreparedRequest], requests.PreparedRequest]
 
 
-class StripeStream(Stream):
+class StripePaginator(BaseOffsetPaginator):
+    def has_more(self, response: Response) -> bool:  # noqa: ARG002
+        """Override this method to check if the endpoint has any pages left.
+
+        Args:
+            response: API response object.
+
+        Returns:
+            Boolean flag used to indicate if the endpoint has more pages.
+        """
+        return response.json()["has_more"]
+
+    def get_next(self, response: Response) -> TPageToken | None:
+        """Get the next pagination token or index from the API response.
+
+        Args:
+            response: API response object.
+
+        Returns:
+            The next page token or index. Return `None` from this method to indicate
+                the end of pagination.
+        """
+        return response.json()["data"][-1]["id"]
+
+
+class StripeStream(RESTStream):
     """Stripe stream class."""
 
-    def __init__(self, tap: Tap):
-        super().__init__(tap)
-        stripe.api_key = self.config.get("api_key")
+    @property
+    def url_base(self) -> str:
+        """Return the API URL root, configurable via tap settings."""
+        return "https://api.stripe.com/v1"
 
-    def get_starting_timestamp(self, context: dict | None) -> int | None:
-        starting_timestamp = self.get_starting_replication_key_value(context)
-        if type(starting_timestamp) == str:
-            gt = int(datetime.timestamp(datetime.strptime(starting_timestamp, "%Y-%m-%dT%H:%M:%SZ")))
-        else:
-            gt = starting_timestamp
-        return gt
+    records_jsonpath = "$.data[*]"  # Or override `parse_response`.
 
+    @property
+    def authenticator(self):
+        """Return the authenticator."""
+        return BasicAuthenticator.create_for_stream(self, username=self.config.get("api_key"), password="")
+
+    def get_url_params(self, context, next_page_token):
+        params = {"limit": 100}
+        start_date = self.get_starting_replication_key_value(context)
+
+        if start_date:
+            if type(start_date) == str:
+                start_date = int(datetime.timestamp(datetime.strptime(start_date, "%Y-%m-%dT%H:%M:%SZ")))
+            params["created[gt]"] = start_date
+
+        if next_page_token:
+            params["starting_after"] = next_page_token
+
+        self.logger.info(f"params = {params}")
+
+        return params
+
+    def get_new_paginator(self) -> BaseOffsetPaginator:
+        """Create a new pagination helper instance.
+
+        If the source API can make use of the `next_page_token_jsonpath`
+        attribute, or it contains a `X-Next-Page` header in the response
+        then you can remove this method.
+
+        If you need custom pagination that uses page numbers, "next" links, or
+        other approaches, please read the guide: https://sdk.meltano.com/en/v0.25.0/guides/pagination-classes.html.
+
+        Returns:
+            A pagination helper instance.
+        """
+        return StripePaginator(page_size=100, start_value=0)
--- tap_stripe/streams.py
+++ tap_stripe/streams.py
@@ -7,18 +7,31 @@ import requests
 from pathlib import Path
 from typing import Iterable, Optional
 from datetime import datetime
-import stripe
 
 from singer_sdk import typing as th  # JSON Schema typing helpers
-from singer_sdk.typing import IntegerType, StringType, DateTimeType, ObjectType, Property, PropertiesList, ArrayType, BooleanType, NumberType
+from singer_sdk.helpers.jsonpath import extract_jsonpath
+from singer_sdk.typing import (
+    IntegerType,
+    StringType,
+    DateTimeType,
+    ObjectType,
+    Property,
+    PropertiesList,
+    ArrayType,
+    BooleanType,
+    NumberType,
+)
 
 from tap_stripe.client import StripeStream
 
 
 class ChargesStream(StripeStream):
     name = "charges"
+    path = "/charges"
     primary_keys: t.ClassVar[list[str]] = ["id"]
-    replication_key = 'created'
+    replication_key = "created"
+    is_sorted = False
+
     schema = PropertiesList(
         Property("id", StringType),
         Property("amount", IntegerType),
@@ -29,68 +42,78 @@ class ChargesStream(StripeStream):
         Property("description", StringType),
         Property("disputed", BooleanType),
         Property("invoice", StringType),
-        Property("metadata", ObjectType(
-            Property("customerId", StringType),
-            Property("paymentFlowId", StringType),
-            Property("fingerprint", StringType)
-        )),
+        Property(
+            "metadata",
+            ObjectType(
+                Property("customerId", StringType),
+                Property("paymentFlowId", StringType),
+                Property("fingerprint", StringType),
+            ),
+        ),
         Property("refunded", BooleanType),
-        Property("status", StringType)
+        Property("status", StringType),
     ).to_dict()
-    
-    def get_records(self, context: Optional[dict]) -> Iterable[dict]:
-        """Return a generator of row-type dictionary objects.
-        """
-        charges = stripe.Charge.list(limit=100, created={'gt':self.get_starting_timestamp(context)})
-        return charges.auto_paging_iter()
+
 
 class DisputesStream(StripeStream):
     name = "disputes"
+    path = "/disputes"
     primary_keys: t.ClassVar[list[str]] = ["id"]
-    replication_key = 'created'
+    is_sorted = False
+    replication_key = "created"
+
     schema = PropertiesList(
         Property("id", StringType),
         Property("amount", IntegerType),
         Property("charge", StringType),
         Property("created", IntegerType),
         Property("currency", StringType),
-        Property("evidence_details", ObjectType(
-            Property("due_by", IntegerType),
-            Property("has_evidence", BooleanType),
-            Property("past_due", BooleanType),
-            Property("submission_count", IntegerType)
-        )),
+        Property(
+            "evidence_details",
+            ObjectType(
+                Property("due_by", IntegerType),
+                Property("has_evidence", BooleanType),
+                Property("past_due", BooleanType),
+                Property("submission_count", IntegerType),
+            ),
+        ),
         Property("is_charge_refundable", BooleanType),
         Property("livemode", BooleanType),
         Property("payment_intent", StringType),
         Property("reason", StringType),
         Property("status", StringType),
     ).to_dict()
-    
-    def get_records(self, context: Optional[dict]) -> Iterable[dict]:
-        """Return a generator of row-type dictionary objects.
-        """
-        disputes = stripe.Dispute.list(limit=100, created={'gt':self.get_starting_timestamp(context)})
-        return disputes.auto_paging_iter()
-    
+
+
 class ExchangeRateStream(StripeStream):
-    name = "exchangerates"
+    name = "exchange_rates"
+    path = "/exchange_rates"
     primary_keys: t.ClassVar[list[str]] = ["id"]
     schema = PropertiesList(
-        Property("receive_currency", StringType),
         Property("send_currency", StringType),
+        Property("receive_currency", StringType),
         Property("rate", NumberType),
-        Property("date", DateTimeType)
+        Property("date", DateTimeType),
     ).to_dict()
-    
-    def get_records(self, context: Optional[dict]) -> Iterable[dict]:
-        """Return a generator of row-type dictionary objects.
+
+    def parse_response(self, response: requests.Response) -> t.Iterable[dict]:
+        """Parse the response and return an iterator of result records.
+
+        Args:
+            response: A raw `requests.Response`_ object.
+
+        Yields:
+            One item for every item found in the response.
+
+        .. _requests.Response:
+            https://requests.readthedocs.io/en/latest/api/#requests.Response
         """
-        exchangerates = stripe.ExchangeRate.list(limit=100)
-        for exchange_rate in exchangerates.auto_paging_iter():
-            for recieve_currency, rate in exchange_rate['rates'].items():
-                yield {'send_currency': exchange_rate['id'],
-                       'receive_currency': recieve_currency,
-                       'rate': rate,
-                       'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                       }
+        data = response.json()["data"]
+        for row in data:
+            for receive_currency, rate in row["rates"].items():
+                yield {
+                    "send_currency": row["id"],
+                    "receive_currency": receive_currency,
+                    "rate": rate,
+                    "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                }
